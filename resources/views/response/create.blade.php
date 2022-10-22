bd16b30288f7713792040fd74829809b354fcf7e
James Geiger <james@lavande.in>
2022-10-22 14:10:30 -0500

    Adds event description and location to RSVP page

--- resources/views/response/create.blade.php
+++ resources/views/response/create.blade.php
@@ -23,7 +23,7 @@
 		@endif
 		<div class="max-w-xl px-6 mx-auto lg:px-8">
 			<h1 class="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">{{ $event->name }}</h1>
-			<div class="flex flex-col mt-1 mb-8 sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-8">
+			<div class="flex flex-col mt-1 sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-8">
 				<div class="flex items-center mt-2 text-sm text-gray-500">
 					<!-- Heroicon name: mini/calendar -->
 					<svg class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -38,6 +38,18 @@
 					</svg>
 					{{ $event->begin_time->format('g:i A') }}
 				</div>
+				<div class="flex items-center mt-2 text-sm text-gray-500">
+					<!-- Heroicon name: mini/map_pin -->
+					<svg class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+						<path fill-rule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clip-rule="evenodd" />
+					</svg>
+					{{ $event->location }}
+				</div>
+			</div>
+			<div class="mt-6 mb-8">
+				@if($event->description)
+				<p class="text-lg font-normal text-gray-900 sm:text-base"> {{ $event->description }}</p>
+				@endif
 			</div>
 			<livewire:response.guest-lookup :event="$event" />
 		</div>
